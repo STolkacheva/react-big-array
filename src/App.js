57b481d04da0a062b4c6a048f8eef3e3d515ae19
s.tolkacheva <s.tolkacheva@systtech.ru>
2023-09-27 15:11:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+const [digits, setDigits] = useState('1,2,3,4,5');
+
+  function addOne(itemStr) {
+  let items = itemStr.split(',')
+    
+  let idx = items.findLastIndex((element) => element < 9);
+
+  if(idx === -1) {
+    items.unshift(0);
+    idx = 0;
+  }
+  
+  items[idx]++;
+  for (let i = idx + 1; i < items.length; i++) {
+    items[i] = 0;
+  }
+  return items.join(',');
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <span>Введите большое число в виде массива </span>
+        <input value={digits} onChange={(e)=>setDigits(e.target.value)}/>
+      </div>
+      <div>
+        <span>Большое число +1,в виде массива</span>
+        <span>{addOne(digits)}</span>
+      </div>
     </div>
   );
 }
-
-export default App;
